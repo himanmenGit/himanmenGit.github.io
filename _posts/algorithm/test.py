d089d7fd09e06978003da85634e491bea41aefc8
himanmen Git <study.himanmen@gmail.com>
2018-05-15 17:22:10 +0900

    알고리즘 0515-약수의 합

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,16 +1,16 @@
 '''
-nextSqaure함수는 정수 n을 매개변수로 입력받습니다.
-n이 임의의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고,
-n이 임의의 정수 x의 제곱이 아니라면 'no'을 리턴하는 함수를 완성하세요.
-예를들어 n이 121이라면 이는 정수 11의 제곱이므로 (11+1)의 제곱인 144를 리턴하고,
-3이라면 'no'을 리턴하면 됩니다
+어떤 수를 입력받아 그 수의 약수를 모두 더한 수 sumDivisor 함수를 완성해 보세요.
+ 예를 들어 12가 입력된다면 12의 약수는 [1, 2, 3, 4, 6, 12]가 되고,
+ 총 합은 28이 되므로 28을 반환해 주면 됩니다.
+
+
 '''
 
 
-import math
-def nextSqure(n):
-    # 함수를 완성하세요
-    return 'no' if not math.sqrt(n).is_integer() else (math.sqrt(n)+1)**2
+def sumDivisor(num):
+    answer = [i for i in range(1, num + 1) if num % i == 0]
+    return sum(answer)
+
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
-print("결과 : {}".format(nextSqure(121)));
+print(sumDivisor(12))
