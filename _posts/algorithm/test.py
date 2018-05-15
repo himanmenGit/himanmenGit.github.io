ad67347951b7df4e905383c604ac4a742f716440
himanmen Git <study.himanmen@gmail.com>
2018-05-16 00:18:53 +0900

    알고리즘 0516-가장긴펠린드롬찾기

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,16 +1,37 @@
-'''
-어떤 수를 입력받아 그 수의 약수를 모두 더한 수 sumDivisor 함수를 완성해 보세요.
- 예를 들어 12가 입력된다면 12의 약수는 [1, 2, 3, 4, 6, 12]가 되고,
- 총 합은 28이 되므로 28을 반환해 주면 됩니다.
+def solution(s):
+    list_s = list(s)
+    if list_s == s[::-1]:
+        return len(list_s)
+
+    result = list()
+    for idx1, value1 in enumerate(list_s):
+        idx2 = 0
+        if value1 in list_s[idx1+1:]:
+            for i, value2 in enumerate(list_s[idx1+1:]):
+                if value2 in list_s[idx1+2:]:
+                    if value1 == value2:
+                        idx2 = idx1 + i + 2
+                    if list_s[idx1:idx2] == list_s[idx1:idx2][::-1]:
+                        result.append(len(list_s[idx1:idx2]))
+
+    if len(result) == 0:
+        return 1
+    return max(result)
+
+def solution(s):
+    if s == s[::-1]:
+        return len(s)
+
+    result = []
+    for i in range(len(s)):
+        for j in range(0, i):
+            chunk = s[j:i + 1]
+
+            if chunk == chunk[::-1]:
+                result.append(len(chunk))
+    if len(result) == 0:
+        return 1
+    return max(result)
 
 
-'''
-
-
-def sumDivisor(num):
-    answer = [i for i in range(1, num + 1) if num % i == 0]
-    return sum(answer)
-
-
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(sumDivisor(12))
+print(solution("abcdcbae"))
