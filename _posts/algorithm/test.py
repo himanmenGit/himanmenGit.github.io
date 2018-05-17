cf80ba0307f7bc62b051e13fa8370313be7f6f25
himanmen Git <study.himanmen@gmail.com>
2018-05-17 12:40:31 +0900

    알고리즘 0517-x만큼 간격이 있는 n개의 숫자

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,8 +1,6 @@
-def strange_sort(strings, n):
-    '''strings의 문자열들을 n번째 글자를 기준으로 정렬해서 return하세요'''
-
-    return sorted(strings, key=lambda strings: strings[n])
-
+def number_generator(x, n):
+    # 함수를 완성하세요
+    return [i * x for i in range(1, n+1)]
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(strange_sort(["sun", "bed", "car"], 1))
+print(number_generator(3, 5))
