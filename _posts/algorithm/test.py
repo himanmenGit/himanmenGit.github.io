941c36b27c26b5b0ce372a9fbb59b8870d7f0f49
himanmen Git <study.himanmen@gmail.com>
2018-05-24 17:15:14 +0900

    알고리즘 0524-두정수사이의합

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,18 +1,1 @@
-def solution(s):
-    if s.count('(') == s.count(')') and s[0] == '(' and s[-1] == ')':
-        count = 0
-        for v in s:
-            if v == '(':
-                count += 1
-            else:
-                if count > 0:
-                    count -= 1
-                else:
-                    return False
-        return True
-    return False
-
-
-print(solution('()()'))
-print(solution(')()('))
-print(solution('())(()'))
+print(sum(range(3, 4)))
