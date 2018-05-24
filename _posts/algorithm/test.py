addb75fb42590d4ffb489dac9922962cae3c9bf5
himanmen Git <study.himanmen@gmail.com>
2018-05-24 16:43:20 +0900

    올바른괄호

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,10 +1,18 @@
-import datetime
+def solution(s):
+    if s.count('(') == s.count(')') and s[0] == '(' and s[-1] == ')':
+        count = 0
+        for v in s:
+            if v == '(':
+                count += 1
+            else:
+                if count > 0:
+                    count -= 1
+                else:
+                    return False
+        return True
+    return False
 
 
-def solution(a, b):
-    t = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-
-    return t[datetime.date(2016, a, b).weekday()]
-
-
-solution(5, 24)
+print(solution('()()'))
+print(solution(')()('))
+print(solution('())(()'))
