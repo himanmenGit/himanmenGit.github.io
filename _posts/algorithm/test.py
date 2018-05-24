b10510675252b21c3a38a20b17ac0d2c7853e5d9
himanmen Git <study.himanmen@gmail.com>
2018-05-24 15:56:57 +0900

    알고리즘 0524-2016년 요일 마추기

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,6 +1,10 @@
-def number_generator(x, n):
-    # 함수를 완성하세요
-    return [i * x for i in range(1, n+1)]
+import datetime
 
-# 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(number_generator(3, 5))
+
+def solution(a, b):
+    t = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+    return t[datetime.date(2016, a, b).weekday()]
+
+
+solution(5, 24)
