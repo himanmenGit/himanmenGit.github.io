585880f5e5bbc456eaf3a1ae9c47e1e4cc327310
himanmen Git <study.himanmen@gmail.com>
2018-05-24 18:26:51 +0900

    알고리즘 0524-소수찾기

--- _posts/algorithm/test.py
+++ _posts/algorithm/test.py
@@ -1,1 +1,11 @@
-print(sum(range(3, 4)))
+def solution(n):
+    answer = list(range(n + 1))
+    for i in range(2, n + 1):
+        if answer[i] == 0:
+            continue
+        for j in range(i + i, n + 1, i):
+            answer[j] = 0
+    return len(list(set(answer))[2:])
+
+
+print(solution(20))
